88a3f6701fd53d48e222dc51afca58f1c7544a01
trusasha <atr@nkhstudio.ru>
2022-06-27 18:36:16 +0400

    GoBack: added animation

--- src/components/GoBack.js
+++ src/components/GoBack.js
@@ -1,45 +1,55 @@
-import React, { useCallback } from "react";
-import { AntDesign } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/core";
-import { View } from "react-native-animatable";
-import { fonts } from "../config/theme";
-import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useCallback } from 'react';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/core';
+import { fonts } from '../config/theme';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 /**
  * @typedef {{
  *  title?: boolean
+ *  style?: import("react-native").ViewStyle
  * }} GoBackParams
  */
 
 /**
  * @param {GoBackParams} props
  */
-export default function GoBack({ title }) {
+export default function GoBack({ title, style }) {
   const navigation = useNavigation();
   const onGoBack = useCallback(() => navigation.goBack(), []);
+  const styles = [S.container];
 
   return (
-    <TouchableOpacity style={S.container} onPress={onGoBack}>
-      <AntDesign name="arrowleft" size={22} color={"#000"} />
-      {title && <Text style={S.textStyle}>{title}</Text>}
-    </TouchableOpacity>
+    <Animatable.View
+      animation={'fadeIn'}
+      duration={700}
+      delay={400}
+      style={[S.absoluteContainer, style]}>
+      <TouchableOpacity style={styles} onPress={onGoBack}>
+        <AntDesign name="arrowleft" size={22} color={'#000'} />
+        {title && <Text style={S.textStyle}>{title}</Text>}
+      </TouchableOpacity>
+    </Animatable.View>
   );
 }
 
 const S = StyleSheet.create({
   container: {
-    backgroundColor: "rgba(255,255,255,0.8)",
+    backgroundColor: 'rgba(255,255,255,0.8)',
     borderRadius: 100,
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     padding: 8,
-    position: "absolute",
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    shadowRadius: 15,
+  },
+  absoluteContainer: {
+    position: 'absolute',
     top: 20,
     left: 20,
     zIndex: 2,
-    shadowColor: "black",
-    shadowOpacity: 0.2,
-    shadowRadius: 15,
   },
   textStyle: { ...fonts.montserratBold, fontSize: 12, marginLeft: 5 },
 });
